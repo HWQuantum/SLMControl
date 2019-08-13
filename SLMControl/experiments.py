3c0c00e0d23d1fd41444bf7609719567c7e14121
Max Tyler <maxastyler@gmail.com>
2019-08-13 18:05:20 +0100

    Make them the opposite

--- SLMControl/experiments.py
+++ SLMControl/experiments.py
@@ -227,7 +227,7 @@ def coincidences_5x5_with_concentration(slm_widget, coincidence_widget,
     overlays.append(
         np.sum([
             a * np.exp(1j *
-                       (l_values[i] *
+                       (-l_values[i] *
                         np.arctan2(y - slm_vals[0][1], x - slm_vals[0][0])))
             for i, a in enumerate(l_norms)
         ],
@@ -235,7 +235,7 @@ def coincidences_5x5_with_concentration(slm_widget, coincidence_widget,
     overlays.append(
         np.sum([
             a * np.exp(1j *
-                       (-l_values[i] *
+                       (l_values[i] *
                         np.arctan2(y - slm_vals[1][1], x - slm_vals[1][0])))
             for i, a in enumerate(l_norms)
         ],
