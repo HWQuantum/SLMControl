cf5bde4e136dc1ec6699967f42885e01e07e05d0
Max Tyler <maxastyler@gmail.com>
2019-08-13 11:16:43 +0100

    Refactor experiments into own function

---
+++ SLMControl/experiments.py
@@ -0,0 +1,102 @@
+'''This file defines functions which take measurements
+'''
+import numpy as np
+from time import sleep
+import pickle
+
+
+class MeasurementReceiver:
+    '''Class used to receive measurement data
+    Stores the received data in a dictionary, with a given key
+    The dictionary is of the form {key: [data]}
+    Where each key has a list of data associated with it
+    '''
+    def __init__(self):
+        self.data = {}
+
+    def set_key(self, new_key):
+        '''Sets the key to store in, converting any lists or dicts to tuples
+        with the tuplise function
+        '''
+        self.key = tuplise(new_key)
+        if self.key not in self.data:
+            self.data[self.key] = []
+
+    def add_data(self, new_data):
+        '''Adds data to the associated key
+        '''
+        self.data[self.key].append(new_data)
+
+    def save_data(self, file):
+        pickle.dump(self.data, file)
+
+
+def tuplise(data):
+    '''Recursively converts any lists/dicts in data into tuples
+    '''
+    if type(data) == list:
+        return tuple(tuplise(i) for i in data)
+    elif type(data) == dict:
+        return tuple((":key:", k, tuplise(v)) for k, v in data.items())
+    else:
+        return data
+
+
+def bell_test_2x2_12345(slm_widget, coincidence_widget, application):
+    '''Takes a 2x2 bell test in each of l=[1, 2, 3, 4, 5]
+    With an integration time of 20s
+    '''
+    integration_time = 20000  # integration time in ms
+    coincidence_window = 3000  # coincidence window in ps
+    histogram_bins = 300  # number of bins for the histogram
+    sync_channel = 0  # the channel the values should be compared with
+
+    measurement_receiver = MeasurementReceiver()
+    angle_a = np.arange(0, 2 * np.pi, np.pi / 2)
+    # angle_b = np.linspace(0, 2 * np.pi, 200)
+    angle_b = np.arange(0 + np.pi / 4, 2 * np.pi, np.pi / 2)
+
+    measurement_receiver.set_key('coincidence_counter_values')
+    measurement_receiver.add_data(coincidence_widget.device_setup.get_values())
+    measurement_receiver.set_key('measurement_parameters')
+    measurement_receiver.add_data({
+        "integration_time": integration_time,
+        "coincidence_window": coincidence_window,
+        "histogram_bins": histogram_bins,
+        "sync_channel": sync_channel,
+    })
+
+    for l in [1, 2, 3, 4, 5]:
+        for i, a in enumerate(angle_a):
+            for j, b in enumerate(angle_b):
+                measurement_receiver.set_key(("l_a_b", l, a, b))
+                slm_widget.set_values([{
+                    "oam_controller": [{
+                        "amplitude": 1,
+                        "ang_mom": l,
+                        "phase": 0,
+                    }, {
+                        "amplitude": 1,
+                        "ang_mom": -l,
+                        "phase": a,
+                    }]
+                }, {
+                    "oam_controller": [{
+                        "amplitude": 1,
+                        "ang_mom": l,
+                        "phase": 0,
+                    }, {
+                        "amplitude": 1,
+                        "ang_mom": -l,
+                        "phase": b,
+                    }]
+                }])
+                application.processEvents()
+                sleep(0.3)
+                measurement_receiver.add_data(
+                    coincidence_widget.measurement_thread.run_measurement_once(
+                        integration_time, coincidence_window, histogram_bins,
+                        sync_channel))
+
+            print("Done {}/{}".format(i + 1, len(angle_a)))
+    return measurement_receiver
--- SLMControl/run_experiment.py
+++ SLMControl/run_experiment.py
@@ -1,14 +1,14 @@
-'''This file contains a widget which wraps up the slm and coincidence counting widgets
+'''This file contains a widget which wraps up the slm and 
+coincidence counting widgets
 and controls them itself to allow taking a run of measurements
 '''
 
 from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QFileDialog
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
 from slm_display import MultiSLMController
 from coincidence_counting import CoincidenceWidget
 from time import sleep
-import pickle
-import numpy as np
+from experiments import bell_test_2x2_12345
 
 
 class ExperimentController(QWidget):
@@ -37,103 +37,13 @@ class ExperimentController(QWidget):
         ''' Runs the experiment, using values set on the device measurement
         page
         '''
-        integration_time = 20000  # integration time in ms
-        coincidence_window = 3000  # coincidence window in ps
-        histogram_bins = 300  # number of bins for the histogram
-        sync_channel = 0  # the channel the values should be compared with
-
-        self.measurement_receiver = MeasurementReceiver()
-        angle_a = np.arange(0, 2 * np.pi, np.pi / 2)
-        # angle_b = np.linspace(0, 2 * np.pi, 200)
-        angle_b = np.arange(0 + np.pi / 4, 2 * np.pi, np.pi / 2)
-
-        self.measurement_receiver.set_key('coincidence_counter_values')
-        self.measurement_receiver.add_data(
-            self.coincidence_widget.device_setup.get_values())
-        self.measurement_receiver.set_key('measurement_parameters')
-        self.measurement_receiver.add_data({
-            "integration_time": integration_time,
-            "coincidence_window": coincidence_window,
-            "histogram_bins": histogram_bins,
-            "sync_channel": sync_channel,
-        })
-
-        for l in [1, 2, 3, 4, 5]:
-            for i, a in enumerate(angle_a):
-                for j, b in enumerate(angle_b):
-                    self.measurement_receiver.set_key(("l_a_b", l, a, b))
-                    self.slm_controller.set_values([{
-                        "oam_controller": [{
-                            "amplitude": 1,
-                            "ang_mom": l,
-                            "phase": 0,
-                        }, {
-                            "amplitude": 1,
-                            "ang_mom": -l,
-                            "phase": a,
-                        }]
-                    }, {
-                        "oam_controller": [{
-                            "amplitude": 1,
-                            "ang_mom": l,
-                            "phase": 0,
-                        }, {
-                            "amplitude": 1,
-                            "ang_mom": -l,
-                            "phase": b,
-                        }]
-                    }])
-                    self.application.processEvents()
-                    sleep(0.3)
-                    self.measurement_receiver.add_data(
-                        self.coincidence_widget.measurement_thread.
-                        run_measurement_once(integration_time,
-                                             coincidence_window,
-                                             histogram_bins, sync_channel))
-
-                print("Done {}/{}".format(i + 1, len(angle_a)))
+        self.measurement_receiver = bell_test_2x2_12345(
+            self.slm_controller, self.coincidence_widget, self.application)
 
         filename, _ = QFileDialog.getSaveFileName(self, "Save file", "")
         if filename:
             with open(filename, 'wb') as f:
                 self.measurement_receiver.save_data(f)
-
-
-class MeasurementReceiver(QObject):
-    '''Class used to receive measurement data
-    Stores the received data in a dictionary, with a given key
-    The dictionary is of the form {key: [data]}
-    Where each key has a list of data associated with it
-    '''
-    def __init__(self):
-        self.data = {}
-
-    def set_key(self, new_key):
-        '''Sets the key to store in, converting any lists or dicts to tuples
-        with the tuplise function
-        '''
-        self.key = tuplise(new_key)
-        if self.key not in self.data:
-            self.data[self.key] = []
-
-    def add_data(self, new_data):
-        '''Adds data to the associated key
-        '''
-        self.data[self.key].append(new_data)
-
-    def save_data(self, file):
-        pickle.dump(self.data, file)
-
-
-def tuplise(data):
-    '''Recursively converts any lists/dicts in data into tuples
-    '''
-    if type(data) == list:
-        return tuple(tuplise(i) for i in data)
-    elif type(data) == dict:
-        return tuple((":key:", k, tuplise(v)) for k, v in data.items())
-    else:
-        return data
 
 
 if __name__ == '__main__':
