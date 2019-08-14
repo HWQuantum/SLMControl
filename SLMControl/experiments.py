1b465a1a7b22d899ac664b89d1c00fb7fc9d8c58
Max Tyler <maxastyler@gmail.com>
2019-08-14 14:08:10 +0100

    Add experiments for 3x3 and 5x5

--- SLMControl/experiments.py
+++ SLMControl/experiments.py
@@ -217,7 +217,7 @@ def coincidences_9x9_with_concentration(slm_widget, coincidence_widget,
 
     # these are the normalised values to do concentration with
     min_l_val = min(l_counts)
-    l_norms = [np.sqrt(min_l_val/i) for i in l_counts]
+    l_norms = [np.sqrt(min_l_val / i) for i in l_counts]
     slm_vals = [(*t.position_controller.get_values(),
                  *t.diffraction_grating.get_values())
                 for t in slm_widget.slm_tabs]
@@ -236,6 +236,196 @@ def coincidences_9x9_with_concentration(slm_widget, coincidence_widget,
         np.sum([
             a * np.exp(1j *
                        (-l_values[i] *
+                        np.arctan2(y - slm_vals[1][1], x - slm_vals[1][0])))
+            for i, a in enumerate(l_norms)
+        ],
+               axis=0))
+
+    for i, o in enumerate(overlays):
+        slm_widget.slm_tabs[i].overlay = o
+
+    for a in l_values:
+        for b in l_values:
+            measurement_receiver.set_key(("l1_l2", a, b))
+            slm_widget.set_values([{
+                "oam_controller": [{
+                    "amplitude": 1,
+                    "ang_mom": a,
+                    "phase": 0,
+                }]
+            }, {
+                "oam_controller": [{
+                    "amplitude": 1,
+                    "ang_mom": b,
+                    "phase": 0,
+                }]
+            }])
+            application.processEvents()
+            sleep(0.3)
+            measurement_receiver.add_data(
+                coincidence_widget.measurement_thread.run_measurement_once(
+                    integration_time, coincidence_window, histogram_bins,
+                    sync_channel))
+
+            print("Done a: {}, b: {}".format(a, b))
+    return measurement_receiver
+
+
+def coincidences_3x3_with_correction(slm_widget, coincidence_widget,
+                                     application):
+    '''Takes the coincidences for l = [-1..1]
+    With an integration time of 10s
+    Add in a modification to the 0 mode to try to concentrate it
+    '''
+    integration_time = 5000  # integration time in ms
+    coincidence_window = 3000  # coincidence window in ps
+    histogram_bins = 300  # number of bins for the histogram
+    sync_channel = 0  # the channel the values should be compared with
+
+    # slm_vals = [(*t.position_controller.get_values(),
+    #              *t.diffraction_grating.get_values())
+    #             for t in slm_widget.slm_tabs]
+    # x, y = np.mgrid[-1:1:500j, -1:1:500j]
+
+    # overlays = [
+    #     np.sum([
+    #         a * np.exp(1j * (l * np.arctan2(y - p_y, x - p_x)))
+    #         for l, a in [(-2, 1), (-1, 0.75), (0, 0.5), (1, 0.75), (2, 1)]
+    #     ],
+    #            axis=0) for p_x, p_y, d_x, d_y in slm_vals
+    # ]
+    # for i, o in enumerate(overlays):
+    #     slm_widget.slm_tabs[i].overlay = o
+
+    measurement_receiver = MeasurementReceiver()
+
+    measurement_receiver.set_key('coincidence_counter_values')
+    measurement_receiver.add_data(coincidence_widget.device_setup.get_values())
+    measurement_receiver.set_key('measurement_parameters')
+    measurement_receiver.add_data({
+        "integration_time": integration_time,
+        "coincidence_window": coincidence_window,
+        "histogram_bins": histogram_bins,
+        "sync_channel": sync_channel,
+    })
+
+    l_values = np.linspace(-1, 1, 3)
+
+    # these are the normalised values to do concentration with
+    slm_vals = [(*t.position_controller.get_values(),
+                 *t.diffraction_grating.get_values())
+                for t in slm_widget.slm_tabs]
+    x, y = np.mgrid[-1:1:500j, -1:1:500j]
+    l_norms = [0.75, 0.5, 0.75]
+
+    overlays = []
+    overlays.append(
+        np.sum([
+            a * np.exp(1j *
+                       (l_values[i] *
+                        np.arctan2(y - slm_vals[0][1], x - slm_vals[0][0])))
+            for i, a in enumerate(l_norms)
+        ],
+               axis=0))
+    overlays.append(
+        np.sum([
+            a * np.exp(1j *
+                       (l_values[i] *
+                        np.arctan2(y - slm_vals[1][1], x - slm_vals[1][0])))
+            for i, a in enumerate(l_norms)
+        ],
+               axis=0))
+
+    for i, o in enumerate(overlays):
+        slm_widget.slm_tabs[i].overlay = o
+
+    for a in l_values:
+        for b in l_values:
+            measurement_receiver.set_key(("l1_l2", a, b))
+            slm_widget.set_values([{
+                "oam_controller": [{
+                    "amplitude": 1,
+                    "ang_mom": a,
+                    "phase": 0,
+                }]
+            }, {
+                "oam_controller": [{
+                    "amplitude": 1,
+                    "ang_mom": b,
+                    "phase": 0,
+                }]
+            }])
+            application.processEvents()
+            sleep(0.3)
+            measurement_receiver.add_data(
+                coincidence_widget.measurement_thread.run_measurement_once(
+                    integration_time, coincidence_window, histogram_bins,
+                    sync_channel))
+
+            print("Done a: {}, b: {}".format(a, b))
+    return measurement_receiver
+
+
+def coincidences_5x5_with_correction(slm_widget, coincidence_widget,
+                                     application):
+    '''Takes the coincidences for l = [-1..1]
+    With an integration time of 10s
+    Add in a modification to the 0 mode to try to concentrate it
+    '''
+    integration_time = 5000  # integration time in ms
+    coincidence_window = 3000  # coincidence window in ps
+    histogram_bins = 300  # number of bins for the histogram
+    sync_channel = 0  # the channel the values should be compared with
+
+    # slm_vals = [(*t.position_controller.get_values(),
+    #              *t.diffraction_grating.get_values())
+    #             for t in slm_widget.slm_tabs]
+    # x, y = np.mgrid[-1:1:500j, -1:1:500j]
+
+    # overlays = [
+    #     np.sum([
+    #         a * np.exp(1j * (l * np.arctan2(y - p_y, x - p_x)))
+    #         for l, a in [(-2, 1), (-1, 0.75), (0, 0.5), (1, 0.75), (2, 1)]
+    #     ],
+    #            axis=0) for p_x, p_y, d_x, d_y in slm_vals
+    # ]
+    # for i, o in enumerate(overlays):
+    #     slm_widget.slm_tabs[i].overlay = o
+
+    measurement_receiver = MeasurementReceiver()
+
+    measurement_receiver.set_key('coincidence_counter_values')
+    measurement_receiver.add_data(coincidence_widget.device_setup.get_values())
+    measurement_receiver.set_key('measurement_parameters')
+    measurement_receiver.add_data({
+        "integration_time": integration_time,
+        "coincidence_window": coincidence_window,
+        "histogram_bins": histogram_bins,
+        "sync_channel": sync_channel,
+    })
+
+    l_values = np.linspace(-2, 2, 5)
+
+    # these are the normalised values to do concentration with
+    slm_vals = [(*t.position_controller.get_values(),
+                 *t.diffraction_grating.get_values())
+                for t in slm_widget.slm_tabs]
+    x, y = np.mgrid[-1:1:500j, -1:1:500j]
+    l_norms = [1, 0.75, 0.5, 0.75, 1]
+
+    overlays = []
+    overlays.append(
+        np.sum([
+            a * np.exp(1j *
+                       (l_values[i] *
+                        np.arctan2(y - slm_vals[0][1], x - slm_vals[0][0])))
+            for i, a in enumerate(l_norms)
+        ],
+               axis=0))
+    overlays.append(
+        np.sum([
+            a * np.exp(1j *
+                       (l_values[i] *
                         np.arctan2(y - slm_vals[1][1], x - slm_vals[1][0])))
             for i, a in enumerate(l_norms)
         ],
