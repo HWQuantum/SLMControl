7331d180b7b1e424efc5f6f3f45af5f15ee4a044
Max Tyler <maxastyler@gmail.com>
2019-10-08 11:22:09 +0100

    Add point_in_circle function

--- SLMControl/circle_coords.py
+++ SLMControl/circle_coords.py
@@ -1,4 +1,4 @@
-'''This file contains the functions that load the circle packing stuff
+'''This file contains the functions that load the pixel entanglement stuff
 The circle packings come from http://hydra.nat.uni-magdeburg.de/packing/csq/csq.html
 The coordinates give the centres of the circles, and they all have the same radius, given in the radii array.
 
@@ -6,6 +6,7 @@ The circles fit into a square with coordinates (-0.5, -0.5) -> (0.5, 0.5)
 '''
 
 import numpy as np
+from numba import njit
 
 radii = np.loadtxt('circle_packing/radius.txt')[:, 1]
 
@@ -16,9 +17,10 @@ def get_coords(i):
     return np.loadtxt('circle_packing/csq{}.txt'.format(i))[:, 1:]
 
 
-def point_in_circle(p, c):
+@njit()
+def point_in_circle(x, y, c):
     '''Check if the given point is inside the given circle
     p is a tuple (x, y) - the point you want to check
     c is a tuple (r, (x, y)) containing the radius of the circle and the x and y
     '''
-    return ((p[0] - c[1][0])**2 + (p[1] - c[1][1])**2) < c[0]**2
+    return ((x - c[1][0])**2 + (y - c[1][1])**2) < c[0]**2
