Fix rotation in pizza slices

SLMControl/pixel_entanglement.py
@@ -56,7 +56,7 @@ def point_in_circle(x, y, c):
 
 
 @njit()
-def point_in_slice(x, y, r, R, theta_1, theta_2, c):
+def point_in_slice(x, y, r, R, theta_1, theta_2, rotation, c):
     '''Check if the given point is inside the given pizza slice
     The pizza slice is defined by two angles (theta_1, theta_2),
     which the angle of the point must be inside.
@@ -64,7 +64,7 @@ def point_in_slice(x, y, r, R, theta_1, theta_2, c):
     r gives a limit to the inside of the circle and
     R gives a limit to the outside of the circle
     '''
-    theta = np.arctan2(y - c[1], x - c[0]) % (np.pi * 2)
+    theta = (np.arctan2(y - c[1], x - c[0]) + rotation) % (np.pi * 2)
     mag = (x - c[0])**2 + (y - c[1])**2
     return (theta_1 <= theta) & (theta < theta_2) & (r**2 < mag) & (mag < R**2)
 
@@ -114,19 +114,19 @@ def pizza_field(components,
     '''
     n_slices = len(components)
     lower_angle = np.linspace(0, 2 * np.pi - (2 / n_slices * np.pi),
-                              n_slices) + slice_angle_spacing + rotation
+                              n_slices) + slice_angle_spacing
     upper_angle = np.linspace(2 / n_slices * np.pi, 2 * np.pi,
-                              n_slices) - slice_angle_spacing + rotation
+                              n_slices) - slice_angle_spacing
     field = np.zeros(X.shape, dtype=np.complex128)
     if n_slices > 1:
         for i, p in enumerate(components):
-            field += p * point_in_slice(X, Y, circle_inner_radius,
-                                        circle_outer_radius, lower_angle[i],
-                                        upper_angle[i], circle_position)
+            field += p * point_in_slice(
+                X, Y, circle_inner_radius, circle_outer_radius, lower_angle[i],
+                upper_angle[i], rotation, circle_position)
     else:
         field += components[0] * point_in_slice(
             X, Y, circle_inner_radius, circle_outer_radius, lower_angle[i],
-            upper_angle[i], circle_position)
+            upper_angle[i], rotation, circle_position)
 
     return field
 
@@ -195,6 +195,235 @@ def nice_circles_plot():
         ax.set_ylim(-1, 1)
         plot_circles(i + 2, ax)
     plt.show()
+
+
+class PizzaEntanglementController(QWidget):
+    '''Controls for pizza entanglement
+    '''
+    def __init__(self, window_title, screens, slm_size):
+        super().__init__()
+        self.screens = screens
+
+        layout = QGridLayout()
+        self.x, self.y = np.mgrid[-1:1:(slm_size[0] * 1j), -1:1:(slm_size[1] *
+                                                                 1j)]
+        self.slm_size = slm_size
+
+        self.overlay = None
+
+        screen_selector = QComboBox()
+        position_zernike_scroll_area = QScrollArea()
+        slm_zernike_scroll_area = QScrollArea()
+
+        # pizza pattern controls
+        self.position = XYController("Pixel Centre")
+        self.diffraction_grating = XYController("Diffraction Grating")
+        self.circle_inner_radius = pg.SpinBox(value=0)
+        self.circle_outer_radius = pg.SpinBox(value=1)
+        self.slice_spacing = pg.SpinBox()
+        self.rotation = pg.SpinBox()
+        self.dim = pg.SpinBox(value=2, int=True, step=1, bounds=(2, None))
+        self.mub_selection = pg.SpinBox(value=0, int=True, step=1)
+        self.basis_selection = pg.SpinBox(value=0, int=True, step=1)
+
+        # Lookup table controls
+        self.lut_control = None
+        self.lut_control_button = QPushButton("Open LUT controls")
+        self.lut_list = [(-np.pi, 0), (np.pi, 255)]
+
+        self.position_zernike_controller = ZernikeSet(
+            self.x,
+            self.y, [(2, 2), (0, 2), (-2, 2)],
+            title="Position Zernike Controller")
+        self.slm_zernike_controller = ZernikeSet(
+            self.x,
+            self.y, [(0, 0), (2, 2), (0, 2), (-2, 2)],
+            title="SLM Zernike Controller")
+
+        position_zernike_scroll_area.setWidget(
+            self.position_zernike_controller)
+        position_zernike_scroll_area.setWidgetResizable(True)
+        slm_zernike_scroll_area.setWidget(self.slm_zernike_controller)
+        slm_zernike_scroll_area.setWidgetResizable(True)
+
+        for i, screen in enumerate(screens):
+            screen_selector.addItem("Screen {}".format(i))
+
+        self.plot = FullScreenPlot(slm_size, slm_size)
+
+        self.slm_window = SLMDisplay(window_title,
+                                     screens[screen_selector.currentIndex()],
+                                     slm_size)
+
+        screen_selector.currentIndexChanged.connect(
+            lambda _: self.slm_window.set_screen(self.screens[screen_selector.
+                                                              currentIndex()]))
+
+        self.circle_inner_radius.sigValueChanged.connect(self.update_image)
+        self.circle_outer_radius.sigValueChanged.connect(self.update_image)
+        self.rotation.sigValueChanged.connect(self.update_image)
+        self.slice_spacing.sigValueChanged.connect(self.update_image)
+        self.dim.sigValueChanged.connect(self.update_image)
+        self.mub_selection.sigValueChanged.connect(self.update_image)
+        self.basis_selection.sigValueChanged.connect(self.update_image)
+        self.position_zernike_controller.value_changed.connect(
+            self.update_image)
+        self.slm_zernike_controller.value_changed.connect(self.update_image)
+        self.diffraction_grating.value_changed.connect(self.update_image)
+        self.position.value_changed.connect(self.update_image)
+        self.position.value_changed.connect(self.change_zernike_position)
+        self.lut_control_button.clicked.connect(self.open_lut_control)
+
+        layout.addWidget(QLabel("Display on:"), 0, 1)
+        layout.addWidget(screen_selector, 0, 2)
+        layout.addWidget(self.diffraction_grating, 0, 3)
+        layout.addWidget(self.plot, 1, 3, 10, 1)
+        layout.addWidget(QLabel("Circle inner radius:"), 1, 0, 1, 1)
+        layout.addWidget(self.circle_inner_radius, 1, 1, 1, 1)
+        layout.addWidget(QLabel("Circle outer radius:"), 2, 0, 1, 1)
+        layout.addWidget(self.circle_outer_radius, 2, 1, 1, 1)
+        layout.addWidget(QLabel("Slice spacing:"), 3, 0)
+        layout.addWidget(self.slice_spacing, 3, 1)
+        layout.addWidget(QLabel("Rotation:"), 4, 0)
+        layout.addWidget(self.rotation, 4, 1)
+        layout.addWidget(QLabel("Dimension:"), 5, 0, 1, 1)
+        layout.addWidget(self.dim, 5, 1, 1, 1)
+        layout.addWidget(QLabel("MUB:"), 6, 0, 1, 1)
+        layout.addWidget(self.mub_selection, 6, 1, 1, 1)
+        layout.addWidget(QLabel("Basis:"), 7, 0, 1, 1)
+        layout.addWidget(self.basis_selection, 7, 1, 1, 1)
+        layout.addWidget(self.lut_control_button, 8, 0, 1, 1)
+        layout.addWidget(self.position, 8, 1, 1, 1)
+        layout.addWidget(position_zernike_scroll_area, 9, 0, 1, 2)
+        layout.addWidget(slm_zernike_scroll_area, 10, 0, 1, 2)
+
+        self.setLayout(layout)
+        self.update_image()
+
+    def open_lut_control(self):
+        '''Open the LUT control if it's not open already
+        '''
+        if self.lut_control is None:
+            self.lut_control = LUTWidget(self.lut_list)
+            self.lut_control.value_changed.connect(self.update_LUT)
+            self.lut_control.closed.connect(self.remove_lut_control)
+            self.lut_control.show()
+
+    def remove_lut_control(self):
+        self.lut_control = None
+
+    @pyqtSlot(list)
+    def update_LUT(self, new_lut):
+        if len(new_lut) >= 2:
+            self.lut_list = new_lut
+            new_lut = points_to_lut(self.lut_list)
+            self.plot.update_LUT(new_lut)
+            self.slm_window.window.update_LUT(new_lut)
+
+    def change_zernike_position(self):
+        x, y = self.position.get_values()
+        self.position_zernike_controller.change_position(
+            self.x - x, self.y - y)
+
+    def update_image(self):
+        '''Update the image displayed on the oam plot and the
+        gui plot
+        '''
+        diff_x, diff_y = self.diffraction_grating.get_values()
+        position_zernike_image = self.position_zernike_controller.get_pattern()
+        slm_zernike_image = self.slm_zernike_controller.get_pattern()
+        x, y = self.position.get_values()
+        pixel_image = pizza_field(
+            basis(self.dim.value(), self.mub_selection.value(),
+                  self.basis_selection.value()), self.x, self.y,
+            self.circle_inner_radius.value(), self.circle_outer_radius.value(),
+            (x, y), self.slice_spacing.value(),
+            self.rotation.value()) * np.exp(
+                1j * (diff_x * self.x + diff_y * self.y))
+        if self.overlay is None:
+            new_image = np.angle(slm_zernike_image * position_zernike_image *
+                                 pixel_image) * np.abs(pixel_image)
+        else:
+            new_image = np.angle(
+                slm_zernike_image * position_zernike_image * pixel_image *
+                self.overlay) * np.abs(pixel_image)
+        self.plot.set_and_update_image(new_image)
+        self.slm_window.set_image(new_image)
+
+    def set_slm_size(self, size):
+        '''Set the size of the slm to display on in pixels
+        size is a tuple, eg: (500, 500)
+        '''
+        self.x, self.y = np.mgrid[-1:1:(size[0] * 1j), -1:1:(size[1] * 1j)]
+        delta_x, delta_y = self.position_controller.get_values()
+        self.slm_window.window.update_SLM_size(size)
+        self.plot.screen_size = size
+        self.plot.update_SLM_size(size)
+        self.position_zernike_controller.generate_polynomials(
+            self.x - delta_x, self.y - delta_y)
+        self.slm_zernike_controller.generate_polynomials(self.x, self.y)
+
+    def close_slm_window(self):
+        self.slm_window.window.close()
+        if self.lut_control is not None:
+            self.lut_control.close()
+
+    def get_values(self):
+        '''Get a dictionary of values for the contained widgets
+        '''
+        return {
+            "position_zernike_controller":
+            self.position_zernike_controller.get_values(),
+            "pixel_radius":
+            self.pixel_radius.value(),
+            "circle_radius":
+            self.circle_radius.value(),
+            "dim":
+            self.dim.value(),
+            "mub_selection":
+            self.mub_selection.value(),
+            "basis_selection":
+            self.basis_selection.value(),
+            "slm_zernike_controller":
+            self.slm_zernike_controller.get_values(),
+            "diffraction_grating":
+            self.diffraction_grating.get_values(),
+            "position":
+            self.position.get_values(),
+            "lut_list":
+            self.lut_list,
+            "overlay":
+            self.overlay,
+        }
+
+    def set_values(self, *args, **kwargs):
+        '''Set the values of the SLM display from a set of args and kwargs
+        Expects values for:
+        position_zernike_controller,
+        slm_zernike_controller,
+        position,
+        diffraction_grating,
+        overlay,
+        lut_list,
+        '''
+        for dictionary in args:
+            for key, value in dictionary.items():
+                if key == 'lut_list':
+                    self.update_LUT(value)
+                else:
+                    try:
+                        getattr(self, key).set_values(value)
+                    except AttributeError:
+                        pass
+
+        for key, value in kwargs.items():
+            if key == 'lut_list':
+                self.update_LUT(value)
+            else:
+                try:
+                    getattr(self, key).set_values(value)
+                except AttributeError:
+                    pass
 
 
 class PixelEntanglementController(QWidget):
@@ -495,7 +724,7 @@ class MultiPixelController(QWidget):
 if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
     app = QApplication([])
-    w = MultiPixelController(app.screens(), [(512, 512), (512, 512)])
-    # w = PixelEntanglementController("hi", app.screens(), (512, 512))
+    # w = MultiPixelController(app.screens(), [(512, 512), (512, 512)])
+    w = PizzaEntanglementController("hi", app.screens(), (512, 512))
     w.show()
     app.exec()