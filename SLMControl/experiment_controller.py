55dba34331d7fb987fe02c502ccb2e6d4696f355
Max Tyler <maxastyler@gmail.com>
2019-10-31 12:14:42 +0000

    Add saving and loading defaults

--- SLMControl/experiment_controller.py
+++ SLMControl/experiment_controller.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QAction
+from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QAction, QFileDialog
 from PyQt5.QtCore import QSettings
 
 from slm_controller import MultiSLMController
@@ -26,10 +26,17 @@ class ExperimentController(QMainWindow):
         save_defaults_action.triggered.connect(self.save_values_to_default)
         load_defaults_action = QAction("Load defaults", self)
         load_defaults_action.triggered.connect(self.load_values_from_default)
+        save_action = QAction("Save values", self)
+        save_action.triggered.connect(self.save_values)
+        load_action = QAction("Load values", self)
+        load_action.triggered.connect(self.load_values)
         main_menu = self.menuBar()
         file_menu = main_menu.addMenu("File")
         file_menu.addAction(save_defaults_action)
         file_menu.addAction(load_defaults_action)
+        file_menu.addSeparator()
+        file_menu.addAction(save_action)
+        file_menu.addAction(load_action)
 
     def closeEvent(self, event):
         self.slm_controller.close()
@@ -47,6 +54,19 @@ class ExperimentController(QMainWindow):
     def save_values_to_default(self):
         settings = QSettings("HWQuantum", "SLMControl")
         settings.setValue("slm_settings", self.slm_controller.get_values())
+
+    def load_values(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "")
+        if filename:
+            settings = QSettings(filename, QSettings.IniFormat)
+            self.set_values(settings.value("slm_settings", {}),
+                            settings.value("coincidence_settings", {}))
+
+    def save_values(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Save file", "")
+        if filename:
+            settings = QSettings(filename, QSettings.IniFormat)
+            settings.setValue("slm_settings", self.slm_controller.get_values())
 
 
 if __name__ == "__main__":
