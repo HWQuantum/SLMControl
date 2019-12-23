f960199726656a091bd920bb005e964b5eee77f4
Max Tyler <maxastyler@gmail.com>
2019-12-23 18:24:03 +0000

    Add a displaying class

---
+++ SLMControl/slm_displayer.py
@@ -0,0 +1,10 @@
+import PyQt5.QtWidgets as qw
+import PyQt5.QtCore as qc
+import PyQt5.QtGui as qg
+
+import pyqtgraph as pg
+
+from square_splitting import Rect
+
+if __name__ == "__main__":
+    pass
