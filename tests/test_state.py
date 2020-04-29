e131c9e127c4e4ece72b00152545fcd2670fa129
Max Tyler <maxastyler@gmail.com>
2020-04-29 13:11:14 +0100

    Add some more tests

--- tests/test_state.py
+++ tests/test_state.py
@@ -7,6 +7,16 @@ import SLMControl.state_validation as sv
 from uuid import uuid4
 
 
+def test_spec_after(func):
+    def wrapper_test_spec(*args, **kwargs):
+        v = func(*args, **kwargs)
+        args[0].assertTrue(sv.slm_controller.is_valid(args[0].state._data),
+                           "state doesn't conform to schema")
+        return v
+
+    return wrapper_test_spec
+
+
 class SLMStateTest(unittest.TestCase):
     def setUp(self):
         """Fill the state with some data
@@ -15,14 +25,19 @@ class SLMStateTest(unittest.TestCase):
         self.screen_id = uuid4()
         self.view_id = uuid4()
         self.pattern_id = uuid4()
+        self.pattern_name = "test_name"
+        self.screen_name = "test_name"
+        self.view_name = "test_name"
+
         self.state._data["patterns"][self.pattern_id] = {
             "id": self.pattern_id,
             "type": "test_type",
-            "name": "test_name"
+            "name": self.pattern_name,
+            "test_prop": 20125,
         }
         self.state._data["views"][self.view_id] = {
             "id": self.view_id,
-            "name": "test_view",
+            "name": self.view_name,
             "transform": {
                 "position": (0, 1),
                 "size": (10, 10),
@@ -32,24 +47,42 @@ class SLMStateTest(unittest.TestCase):
         }
         self.state._data["screens"][self.screen_id] = {
             "id": self.screen_id,
-            "name": "test_screen",
+            "name": self.screen_name,
             "size": (0, 1),
             "offset": (10, 10),
             "views": {}
         }
+        self.state._data["additional_data"] = 2
 
+    @test_spec_after
     def test_get_screen_by_uuid(self):
         self.assertEqual(self.state.get_screen_by_uuid(self.screen_id),
                          self.state._data["screens"][self.screen_id])
-        self.assertTrue(sv.slm_screen.validate(self.state._data["screens"][self.screen_id]))
 
+    @test_spec_after
     def test_get_view_by_uuid(self):
         self.assertEqual(self.state.get_view_by_uuid(self.view_id),
                          self.state._data["views"][self.view_id])
 
+    @test_spec_after
     def test_get_pattern_by_uuid(self):
         self.assertEqual(self.state.get_pattern_by_uuid(self.pattern_id),
                          self.state._data["patterns"][self.pattern_id])
+
+    @test_spec_after
+    def test_get_pattern_by_name(self):
+        self.assertEqual(self.state.get_pattern_by_name(self.pattern_name),
+                         self.state.get_pattern_by_uuid(self.pattern_id))
+
+    @test_spec_after
+    def test_get_view_by_name(self):
+        self.assertEqual(self.state.get_view_by_name(self.view_name),
+                         self.state.get_view_by_uuid(self.view_id))
+
+    @test_spec_after
+    def test_get_screen_by_name(self):
+        self.assertEqual(self.state.get_screen_by_name(self.screen_name),
+                         self.state.get_screen_by_uuid(self.screen_id))
 
 
 if __name__ == "__main__":
