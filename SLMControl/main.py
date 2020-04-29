58fad7cab1a603d945586df93de71ba65762110a
Max Tyler <maxastyler@gmail.com>
2020-04-29 16:02:41 +0100

    Update the main screen to have some docking in it

--- SLMControl/main.py
+++ SLMControl/main.py
@@ -1,13 +1,38 @@
 import PyQt5.QtCore as qc
 import PyQt5.QtWidgets as qw
 import sys
+from SLMControl.view import View
+from SLMControl.screen import Screen
+from SLMControl.state import SLMState
+from uuid import uuid4
+
 
 class MainWindow(qw.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.state = SLMState()
+        self.state.add_view({
+            "id": uuid4(),
+            "name": "test_view",
+            "patterns": {},
+            "transform": {
+                "position": (0, 0),
+                "size": (0, 0),
+                "rotation": 0
+            }
+        })
+        self.state.add_screen({
+            "id": uuid4(),
+            "name": "test_screen",
+            "offset": (0, 0),
+            "views": {}
+        })
         layout = qw.QHBoxLayout()
         button = qw.QPushButton("Add new qdock")
         button.clicked.connect(self.add_dock)
+        layout.addWidget(button)
+        button = qw.QPushButton("Add new screen")
+        button.clicked.connect(self.add_screen)
         layout.addWidget(button)
         w = qw.QWidget()
         w.setLayout(layout)
@@ -15,10 +40,18 @@ class MainWindow(qw.QMainWindow):
 
     def add_dock(self):
         w = qw.QDockWidget(self)
-        button = qw.QPushButton("Push me")
-        w.setWidget(button)
+
+        v = View(self.state.get_view_by_name("test_view"), self.state)
+        w.setWidget(v)
         self.addDockWidget(qc.Qt.RightDockWidgetArea, w)
-        
+
+    def add_screen(self):
+        w = qw.QDockWidget(self)
+
+        v = Screen(self.state.get_screen_by_name("test_screen"), self.state)
+        w.setWidget(v)
+        self.addDockWidget(qc.Qt.RightDockWidgetArea, w)
+
 
 class buttonThing(qw.QDockWidget):
     def __init__(self, *args, **kwargs):
@@ -27,6 +60,7 @@ class buttonThing(qw.QDockWidget):
         button = qw.QPushButton("This is a button")
         layout.addWidget(button)
         self.setLayout(layout)
+
 
 app = qw.QApplication(sys.argv)
 
